2c4a0105e49b2693ef08eb4a959f8d8501d6529b
Tony <anton.semionov1@gmail.com>
2020-11-19 19:12:26 +0200

    feat: add single.php

---
+++ wp-content/themes/movie-world/single.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+    <div class="row">
+    <div class="col-sm-8 blog-main">
+    <div class="blog-post">
+        <h2 class="blog-post-title"><?php the_title() ?></h2>
+        <p class="blog-post-meta"><?php the_date() ?> <a href="#"><?php the_author() ?></a></p>
+        <?php the_content() ?>
+    </div>
+    </div>
+<?php get_sidebar() ?>
+    </div>
+<?php comments_template() ?>
+<?php get_footer() ?>
