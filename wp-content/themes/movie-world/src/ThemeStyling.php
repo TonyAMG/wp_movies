c5b47b9b68ef68e351c278c21d5d842cd4cc0969
Tony <anton.semionov1@gmail.com>
2020-12-22 16:20:41 +0200

    improvement: now slick slider uses remote js library now it uses external slick.js from official slick-slider CDN, file slider.js removed from this repository

--- wp-content/themes/movie-world/src/ThemeStyling.php
+++ wp-content/themes/movie-world/src/ThemeStyling.php
@@ -3,11 +3,11 @@
 add_action('wp_enqueue_scripts', 'movie_world_scripts');
 function movie_world_scripts()
 {
-    wp_enqueue_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.css');
+    wp_enqueue_style('bootstrap','//stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.css');
     wp_enqueue_style('theme-style', get_stylesheet_uri());
     wp_enqueue_style('movie-details-page', get_template_directory_uri() . '/assets/css/movie-details-page.css');
     wp_enqueue_style('slick-slider-style', get_template_directory_uri() . '/assets/slick-slider/css/slick.css');
     wp_enqueue_style('slick-slider-theme', get_template_directory_uri() . '/assets/slick-slider/css/slick-theme.css');
-    wp_enqueue_script('slick-slider', get_template_directory_uri() . '/assets/slick-slider/js/slick.js', ['jquery']);
+    wp_enqueue_script('slick-slider', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.js', ['jquery']);
     wp_enqueue_script('slider-init', get_template_directory_uri() . '/assets/slick-slider/js/slider-init.js', ['slick-slider'], null, true);
 }
